f8d72361a10edef2cec947a3281c7f4d203699e3
Rohan Gaonkar <gaonkar70@gmail.com>
2021-01-18 13:35:23 +0530

    heroku

--- server/index.js
+++ server/index.js
@@ -7,4 +7,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./router/route"));
-app.listen(5000, () => console.log("server started on port 5000"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("server started on port 5000"));
